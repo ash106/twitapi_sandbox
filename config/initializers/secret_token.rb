d60541510a2b316bc8b482c00681dc271eb5b559
Alex Howington <alex.howington@gmail.com>
2013-11-26 18:55:01 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitapiSandbox::Application.config.secret_token = 'f5afba4f1fea24110c647af74bb3798acfc242e116d412408895a5ed6d604713ac3932218a4f8073bd6cf7aa4ed4b9647537e4c645b7241359700cd40ac60149'
